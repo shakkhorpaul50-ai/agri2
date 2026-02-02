be2548d683c34b5efa7159b2b311d6634690265e
shakkhorpaul50-ai <shakkhorpaul50@gmail.com>
2026-02-03 01:54:12 +0600

    Update db.ts

--- services/db.ts
+++ services/db.ts
@@ -5,12 +5,13 @@ import { User, Field, Sensor } from '../types';
 
 // Configuration for project: agricare-4c725
 const firebaseConfig = {
-  apiKey: "AIzaSyCeyl_T15XCsu0-tbXoXaZ2t7C3oMLjyF8",
-  authDomain: "agricare-4c725.firebaseapp.com",
-  projectId: "agricare-4c725",
-  storageBucket: "agricare-4c725.appspot.com",
-  messagingSenderId: "629410782904",
-  appId: "1:629410782904:web:4d8f43225d8a6b4ad15e4d"
+  apiKey: "AIzaSyAbaTQEh_fGcjUNqanjIr4lhQAhDxN8zts",
+  authDomain: "agricare-1ae26.firebaseapp.com",
+  projectId: "agricare-1ae26",
+  storageBucket: "agricare-1ae26.firebasestorage.app",
+  messagingSenderId: "11657101745",
+  appId: "1:11657101745:web:1469eb8c2a77066babbd90",
+  measurementId: "G-XSPX255MSL"
 };
 
 // Initialize app safely
